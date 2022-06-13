c09a30ff34ab5020ea6df83d2879357343f86ac6
gigi.upNorth <leung.gerry@gmail.com>
2022-06-13 08:23:09 -0400

    [FE] Add getting user input for character length of password

--- res/script.js
+++ res/script.js
@@ -1,15 +1,46 @@
-// Assignment code here
+/**
+ * Function Defintions
+ **************************************************************************************************/
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+/**
+ * 
+ * @returns 
+ */
+// Assignment code here
+function getLength() {
+  var length = window.prompt(
+    "Please choose the length of your password (choose between 8 characters and 128 characters):"
+  );
+
+  // if password is less than 8 chars or greater than 128
+  // alert the user to enter a valid input
+  if (length < 8 || length > 128) {
+    window.alert(length + " is an invalid password length. Please choose again");
+    getLength();
+  }
+  return length;
+}
+
+
+function generatePassword() {
+  var passLen = getLength();
+
+  console.log(passLen);
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  }
+/**
+ * Main
+ **************************************************************************************************/
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
 
-  passwordText.value = password;
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
